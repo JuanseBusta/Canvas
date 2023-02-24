ba9d1a9a024ba3d4698388a166f0f7e136565fe0
JuanseBusta <piolo180595@gmail.com>
2023-02-24 12:24:56 -0500

    First line

--- canvas.js
+++ canvas.js
@@ -1,3 +1,9 @@
 let cuadroCanvas = document.getElementById('dibujo')
-let lienzo =cuadroCanvas.getcontext('2d')
+let lienzo =cuadroCanvas.getContext('2d')
 
+lienzo.beginPath();
+lienzo.strokeStyle = "red"
+lienzo.moveTo(30,80)
+lienzo.lineTo(250,60)
+lienzo.stroke()
+lienzo.closePath
