7eae8ad8c11a8b5bfa14caf20903b059e34d73a5
JuanseBusta <piolo180595@gmail.com>
2023-02-27 05:46:08 -0500

    function to draw lines

--- canvas.js
+++ canvas.js
@@ -1,9 +1,15 @@
 let cuadroCanvas = document.getElementById('dibujo')
 let lienzo =cuadroCanvas.getContext('2d')
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red"
-lienzo.moveTo(30,80)
-lienzo.lineTo(250,60)
-lienzo.stroke()
-lienzo.closePath
+dibujarLineas("red",10,20,287,150)
+dibujarLineas("gray",90,268,40,15)
+
+function dibujarLineas(color,xInicial,yInicial,xFinal,yFinal){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color
+    lienzo.moveTo(xInicial,yInicial)
+    lienzo.lineTo(xFinal,yFinal)
+    lienzo.stroke()
+    lienzo.closePath
+}
+
